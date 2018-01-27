3ae6914fc856d5cfb97192a792ee22dedbbb5768
devapp15 <devapp15@gmail.com>
2018-01-28 02:27:18 +0700

    lesson 02

--- app/src/main/java/com/trvrx/coderswag/Model/Category.kt
+++ app/src/main/java/com/trvrx/coderswag/Model/Category.kt
@@ -3,4 +3,8 @@ package com.trvrx.coderswag.Model
 /**
  * Created by User on 27/1/2561.
  */
-class Category(val title: String, val image: String)
+class Category(val title: String, val image: String) {
+    override fun toString(): String {
+        return title
+    }
+}
